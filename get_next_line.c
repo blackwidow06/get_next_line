174b0fc7675c19e7ca42c0c2bcdce5b8b91403a4
Maëva <maevalavaud@icloud.com>
2025-10-31 12:32:15 +0100

    Add files via upload

---
+++ get_next_line.c
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malavaud <malavaud@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/24 16:42:03 by malavaud          #+#    #+#             */
+/*   Updated: 2025/10/31 12:29:20 by malavaud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_line(char *stash)
+{
+	int		i;
+	char	*line;
+
+	if (!stash || !stash[0])
+		return (NULL);
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	if (stash[i] == '\n')
+		line = ft_substr(stash, 0, i + 1);
+	else
+		line = ft_substr(stash, 0, i);
+	return (line);
+}
+char	*clean_stash(char *stash)
+{
+	int		i;
+	char	*new;
+
+	if (!stash)
+		return (NULL);
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	if (!stash[i])
+	{
+		free(stash);
+		return (NULL);
+	}
+	new = ft_strdup(stash + i + 1);
+	free(stash);
+	return (new);
+}
+
+char	*ft_read(int fd, char *stash)
+{
+	char	*buffer;
+	size_t	bytes;
+	char	*tmp;
+
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	bytes = 1;
+	while (!ft_strchr(stash, '\n') && bytes > 0)
+	{
+		bytes = read(fd, buffer, BUFFER_SIZE);
+		if (bytes < 0)
+		{
+			free(buffer);
+			free(stash);
+			return (NULL);
+		}
+		buffer[bytes] = '\0';
+		tmp = ft_strjoin(stash, buffer);
+		free(stash);
+		stash = tmp;
+	}
+	free(buffer);
+	return (stash);
+}
+char	*get_next_line(int fd)
+{
+	static char	*stash;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	stash = ft_read(fd, stash);
+	if (!stash)
+		return (NULL);
+	line = ft_line(stash);
+	stash = clean_stash(stash);
+	return (line);
+}
+
+int	main(void)
+{
+	int	fd;
+	char	*line;
+
+	fd = open("text.txt", O_RDONLY);
+	if (fd < 0)
+		return (0);
+	while ((line = get_next_line(fd)))
+	{
+		printf("%s", line);
+		free(line);
+	}
+	close(fd);
+	return (0);
+}
